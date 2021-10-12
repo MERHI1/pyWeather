07c7348500b151845cfae882f3e5ec95071b8ab7
MERHI <mihail.pateev@yandex.ru>
2021-10-12 13:48:34 +0300

    add code v.2

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from data_file import api
 owm = pyowm.OWM(api)
 mgr = owm.weather_manager()
 
-observation = mgr.weather_at_place('London')
+city = input("Название города: ")
+
+observation = mgr.weather_at_place(city)
 w = observation.weather
 
 temp = w.temperature('celsius')['temp']
